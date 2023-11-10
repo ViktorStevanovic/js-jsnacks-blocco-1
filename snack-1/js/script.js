39dc8b10b9f54ff4feeb7539bba24e7962c77f8b
ViktorStevanovic <stevanovic.lavoro@gmail.com>
2023-11-10 11:50:18 +0100

    Chiedere l'età di due persone e comunicare qale delle due è più grande

---
+++ snack-1/js/script.js
@@ -0,0 +1,15 @@
+// chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
+
+
+const firstUserAge = parseInt(prompt('Quanti anni ha Marco?'))
+const secondUserAge = parseInt(prompt('Quanti anni ha Viktor?'))
+
+if (firstUserAge > secondUserAge) {
+    console.log('Marco è il più grande')
+}
+else if (secondUserAge > firstUserAge) {
+    console.log('Viktor è il più grande')
+}
+else {
+    console.log ('Marco e Viktor hanno la stessa età')
+}
